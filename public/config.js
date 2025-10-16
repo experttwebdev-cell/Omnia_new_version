a69a880ce24ed965162c367d2453f5ce38d42c67
obenrockman-source <oben.rockman@gmail.com>
2025-10-16 17:39:24 +0200

    Updated config.js

--- public/config.js
+++ public/config.js
@@ -4,5 +4,5 @@
 window.ENV = {
   VITE_SUPABASE_URL: 'https://ufdhzgqrubbnornjdvgv.supabase.co',
   VITE_SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVmZGh6Z3FydWJibm9ybmpkdmd2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA0MjAwMzksImV4cCI6MjA3NTk5NjAzOX0.Xqfe56k9FU-85RBv9h1cxq1UvLU1tUsg24MAdzCqZms',
-  VITE_OPENAI_API_KEY: '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'
+  VITE_OPENAI_API_KEY: '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'
 };
